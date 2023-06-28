cce612a6b9dc05da188a57c3cf63b0562cc47a61
Chart <chart.asa@gmail.com>
2023-06-28 23:11:41 +0700

    update data structure for creating report

--- app/imageProcedure.py
+++ app/imageProcedure.py
@@ -2,11 +2,11 @@ from PIL import Image, ImageDraw
 
 def getImage(message: str, imgtype: str):
     if imgtype == 'wait':
-        img = Image.open('images/waiting.jpg')
+        img = Image.open('./static/images/waiting.jpg')
     elif imgtype == 'pass':
-        img = Image.open('images/passImg.jpg')
+        img = Image.open('./static/images/passImg.jpg')
     else:
-        img = Image.open('images/NotAllowed.jpg')
+        img = Image.open('./static/images/NotAllowed.jpg')
     dl = ImageDraw.Draw(img)
     dl.text((36, 36), message, fill=(255,255,255))
     return img 
